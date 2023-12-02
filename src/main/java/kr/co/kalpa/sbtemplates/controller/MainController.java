ee60537d814c69a8a6c9c1df3277ff565aac1c86
KimDoYoung <kdy987@gmail.com>
2023-12-02 23:38:25 +0900

    jquery, log

--- src/main/java/kr/co/kalpa/sbtemplates/controller/MainController.java
+++ src/main/java/kr/co/kalpa/sbtemplates/controller/MainController.java
@@ -3,11 +3,17 @@ package kr.co.kalpa.sbtemplates.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import lombok.extern.slf4j.Slf4j;
+
 @Controller
+@Slf4j
 public class MainController {
 
 	@GetMapping("/")
 	public String main() {
+		log.debug("****************************************");
+		log.debug("Main...");
+		log.debug("****************************************");
 		return "main";
 		
 	}
--- src/main/webapp/WEB-INF/view/main.jsp
+++ src/main/webapp/WEB-INF/view/main.jsp
@@ -8,5 +8,12 @@
 </head>
 <body>
 <h1>TEST</h1>
+
+<script src="https://code.jquery.com/jquery-3.7.1.min.js" integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo=" crossorigin="anonymous"></script>
+<script>
+$(document).ready(function(){
+	console.log('ready...');
+});
+</script>
 </body>
 </html>
