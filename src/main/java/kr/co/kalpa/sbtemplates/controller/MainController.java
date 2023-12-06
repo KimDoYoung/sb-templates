99bc53b79fefe4f43f75f9ce40d368ab281a5825
KimDoYoung <kdy987@gmail.com>
2023-12-06 10:10:05 +0900

    다듬기

--- src/main/java/kr/co/kalpa/sbtemplates/controller/MainController.java
+++ src/main/java/kr/co/kalpa/sbtemplates/controller/MainController.java
@@ -6,10 +6,8 @@ import java.util.List;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -21,21 +19,20 @@ import kr.co.kalpa.sbtemplates.view.JsonView;
 @Controller
 public class MainController {
 
+	List<User> users =  new ArrayList<User>( Arrays.asList(
+			new User("1","Hong"),
+			new User("2","Gil"),
+			new User("3","Dong")
+	));
+
 	@GetMapping("/")
 	public String main() {
 		return "main";
 	}
-	List<User> users =  new ArrayList<User>( Arrays.asList(
-			new User("1","Hong"),new User("2","Gil"),new User("3","Dong")
-			));
 	@ResponseBody
 	@GetMapping("ajax2")
 	public String ajax2() {
 		
-//		List<User> users = new ArrayList<User>();
-//		users.add(new User("1","Hong"));
-//		users.add(new User("2","Gil"));
-//		users.add(new User("3","Dong"));
 		Gson gson = new Gson(); 
 		String json = gson.toJson(users); 
 		return json;
@@ -44,10 +41,6 @@ public class MainController {
 	@PostMapping("ajax3")
 	public ModelAndView ajax3(@RequestBody User user) {
 		
-//		List<User> users = new ArrayList<User>();
-//		users.add(new User("1","Hong"));
-//		users.add(new User("2","Gil"));
-//		users.add(new User("3","Dong"));
 		users.add(new User(user.getId(), user.getName()));
 		
 		ModelAndView mav = new ModelAndView(new JsonView());
